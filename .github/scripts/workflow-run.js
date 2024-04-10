92602fba0fdf3f05ee99492e1753239bb8f8de5d
Erasmus Cedernaes <erasmus.cedernaes@intel.com>
2024-04-10 08:53:57 +0200

    Add more logging

--- .github/scripts/workflow-run.js
+++ .github/scripts/workflow-run.js
@@ -12,7 +12,7 @@ module.exports = async ({github, context, core}) => {
             workflow_id,
             branch: "main", // Adjust this as necessary
         });
-        console.log(runs);
+        console.log(JSON.stringify(runs.data));
         const recentRun = runs.data.workflow_runs.find(run => run.head_sha === sha);
         console.log(recentRun);
         if (!recentRun || recentRun.conclusion !== 'success') {
